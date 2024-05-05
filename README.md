5cfe35a805dec9ed36b5138d830a424a6425695c
Guy Cole <guycole@gmail.com>
2024-05-04 17:39:57 -0700

    working nginx/gunicorn/django example

--- README.md
+++ README.md
@@ -3,3 +3,8 @@ nginx diagnostic container
 
 + nginx in docker 
 + hints in src/Dockerfile
+
+/app/django/manage.py
+/app/django/spatula
+
+django on port 8000
---
+++ src/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Title:entrypoint.sh
+# Description: 
+# Development Environment: debian 11 bullseye
+#
+RUN_PORT="8000"
+#
+#/usr/bin/python3 django/manage.py migrate --no-input
+cd /app/django
+/usr/local/bin/gunicorn spatula.wsgi:application --bind "0.0.0.0:${RUN_PORT}" --daemon
+#/usr/local/bin/gunicorn spatula.wsgi:application --bind "0.0.0.0:8000" --daemon
+#
+/usr/sbin/nginx -g 'daemon off;'
+#
